3da44624528a32d0c87a27ba345af9f2d32a3a01
bitfede <f.g.defaveri@gmail.com>
2021-08-03 01:28:56 -0400

    Builds the first version of the landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Dropmints`,
+    description: `Welcome to Dropmints!`,
+    author: `@dropmints`,
+    siteUrl: `https://dropmints.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/app-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,50 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import twitterLogo from '../images/twitter-logo-white.svg';
+import dropmintsLogo from '../images/dropmints-white-logo-full.svg';
+
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <Seo title="Coming Soon" />
+    
+    <div className="flex flex-col h-screen min-h-5/6">
+      <div className="h-5/6 flex flex-col justify-center items-center" id="topsection">
+
+        <a href="https://twitter.com/dropmints" className="block md:hidden mb-auto mt-8 md:mt-0">
+          <img className="w-8" src={twitterLogo} alt="the twitter logo" />
+        </a>
+
+        <img draggable="false" className="biglogo w-96 mb-auto md:mb-0 select-none" src={dropmintsLogo} alt="the logo of Dropmints" />
+      </div>
+
+      <div className="h-1/6 flex flex-col-reverse md:flex-row justify-between items-center" id="bottomsection">
+
+        <a href="https://twitter.com/dropmints" className="hidden md:block md:mb-0 md:ml-16 hover:fill-blue-800 transform hover:bg-blue-500 rounded-full hover:bg-opacity-50 transition duration-500 hover:scale-110">
+          <img className="w-10 md:w-10 m-4" src={twitterLogo} alt="the twitter logo" />
+        </a>
+
+        <form action="https://formspree.io/f/meqvrlan" method="POST" className="mb-12 md:mb-8 md:mr-16">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col">
+              <label className="mb-4 text-white text-xl font-bold" htmlFor="join-input">Sign up and stay in the loop.</label>
+            </div>
+            <div>
+              <input name="dropmintsform" id="join-input" type='text'  placeholder="email@example.com" className="bg-transparent border-solid border-2 border-white px-2 py-2 placeholder-gray-100 rounded-lg text-white focus:outline-none" required />
+              <button type="submit" className="ml-4 bg-transparent border-solid border-2 border-white px-2 py-2 rounded-lg shadow text-white focus:outline-white">Join</button>
+            </div>
+          </div>
+        </form>
+        
+      </div>
+
+
+    </div>
+
   </Layout>
 )
 
